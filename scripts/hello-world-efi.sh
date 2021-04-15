fe6a05932a390d970af28bdb2258bb47f83dcc57
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-04-15 10:20:56 -0700

    Replace with builtin script

--- scripts/hello-world-efi.sh
+++ scripts/hello-world-efi.sh
@@ -5,12 +5,4 @@
 export DISPLAY=host.docker.internal:0
 
 cd $HOME/mikanos/helloworld/asm
-qemu-img create -f raw disk.img 200M
-mkfs.fat -n 'MIKAN OS' -s 2 -f 2 -R 32 -F 32 disk.img
-
-mkdir -p mnt
-sudo mount -o loop disk.img mnt
-sudo mkdir -p mnt/EFI/BOOT
-sudo cp BOOTX64.EFI mnt/EFI/BOOT/BOOTX64.EFI
-sudo umount mnt
-qemu-system-x86_64 -drive if=pflash,file=$HOME/osbook/devenv/OVMF_CODE.fd -drive if=pflash,file=$HOME/osbook/devenv/OVMF_VARS.fd -hda disk.img
+$HOME/osbook/devenv/run_qemu.sh BOOTX64.EFI
