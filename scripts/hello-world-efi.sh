c9037f465cad7bfdf9d04abdabf364ca23a32664
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-04-15 00:29:33 -0700

    Add commands to show hello world with EFI binary in XQuartz

---
+++ scripts/hello-world-efi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run with vscode user!
+
+export DISPLAY=host.docker.internal:0
+
+cd ~/osbook/day01/bin
+qemu-img create -f raw disk.img 200M
+mkfs.fat -n 'MIKAN OS' -s 2 -f 2 -R 32 -F 32 disk.img
+
+mkdir -p mnt
+sudo mount -o loop disk.img mnt
+sudo mkdir -p mnt/EFI/BOOT
+sudo cp hello.efi mnt/EFI/BOOT/BOOTX64.EFI
+sudo umount mnt
+qemu-system-x86_64 -drive if=pflash,file=$HOME/osbook/devenv/OVMF_CODE.fd -drive if=pflash,file=$HOME/osbook/devenv/OVMF_VARS.fd -hda disk.img
