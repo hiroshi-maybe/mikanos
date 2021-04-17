6e6a7a389d53ef1cef2f4f72667384f652443ac3
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-04-16 23:40:42 -0700

    Add hello world written in C

--- scripts/hello-world-efi.sh
+++ scripts/hello-world-efi.sh
@@ -4,5 +4,10 @@
 
 export DISPLAY=host.docker.internal:0
 
-cd $HOME/mikanos/helloworld/asm
-$HOME/osbook/devenv/run_qemu.sh BOOTX64.EFI
+# cd $HOME/mikanos/helloworld/asm
+# $HOME/osbook/devenv/run_qemu.sh BOOTX64.EFI
+
+cd $HOME/mikanos/helloworld/c
+clang -target x86_64-pc-win32-coff -mno-red-zone -fno-stack-protector -fshort-wchar -Wall -c hello.c
+lld-link /subsystem:efi_application /entry:EfiMain /out:hello.efi hello.o
+$HOME/osbook/devenv/run_qemu.sh hello.efi
