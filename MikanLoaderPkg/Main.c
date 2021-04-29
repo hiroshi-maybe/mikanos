98c840c63bb2b565ec69f947f44fcb14c05b44cf
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-04-28 23:02:00 -0700

    Successfully load built kernel with UEFI loader

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -6,6 +6,7 @@
 #include <Protocol/SimpleFileSystem.h>
 #include <Protocol/DiskIo2.h>
 #include <Protocol/BlockIo.h>
+#include <Guid/FileInfo.h>
 
 // #@@range_begin(struct_memory_map)
 struct MemoryMap {
@@ -110,8 +111,6 @@ EFI_STATUS EFIAPI UefiMain(
 {
     Print(L"Hello, Mikan World!\n");
 
-    // #@@range_begin(main)
-
     CHAR8 memmap_buf[4096 * 4];
     struct MemoryMap memmap = { sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0 };
     GetMemoryMap(&memmap);
@@ -126,7 +125,60 @@ EFI_STATUS EFIAPI UefiMain(
     SaveMemoryMap(&memmap, memmap_file);
     memmap_file->Close(memmap_file);
 
-    // #@@range_end(main)
+    // #@@range_begin(read_kernel)
+
+    EFI_FILE_PROTOCOL* kernel_file;
+    root_dir->Open(root_dir, &kernel_file, L"\\kernel.elf", EFI_FILE_MODE_READ, 0);
+
+    UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+    UINT8 file_info_buffer[file_info_size];
+    kernel_file->GetInfo(kernel_file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
+
+    EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
+    UINTN kernel_file_size = file_info->FileSize;
+
+    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+    // The size of a page is fixed in ELF
+    const int SIZE_OF_PAGE = 0x1000;
+    gBS->AllocatePages(
+        AllocateAddress, EfiLoaderData, (kernel_file_size + SIZE_OF_PAGE - 1) / SIZE_OF_PAGE, &kernel_base_addr);
+    kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+    Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+
+    // #@@range_end(read_kernel)
+
+    // #@@range_begin(exit_bs)
+
+    EFI_STATUS status;
+    status = gBS->ExitBootServices(image_handle, memmap.map_key);
+    if (EFI_ERROR(status)) {
+        status = GetMemoryMap(&memmap);
+        if (EFI_ERROR(status)) {
+            Print(L"failed to get memory map: %r\n", status);
+            while(1);
+        }
+        status = gBS->ExitBootServices(image_handle, memmap.map_key);
+        if (EFI_ERROR(status)) {
+            Print(L"Could not exit boot service: %r\n", status);
+            while(1);
+        }
+    }
+
+    // #@@range_end(exit_bs)
+
+    // #@@range_begin(call_kernel)
+
+    // The offset for 64bit system defined in ELF
+    const int OFFSET_OF_ENTRYPOINT_ADDRESS = 24;
+    UINT64 entry_addr = *(UINT64*)(kernel_base_addr + OFFSET_OF_ENTRYPOINT_ADDRESS);
+
+    typedef void EntryPointType(void);
+    EntryPointType* entry_point = (EntryPointType*)entry_addr;
+    entry_point();
+
+    // #@@range_end(call_kernel)
+
+    Print(L"All done\n");
 
     while (1);
     return EFI_SUCCESS;
---
+++ scripts/load-kernel.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Build the bootloader
+# https://github.com/uchan-nos/mikanos-build#%E3%83%96%E3%83%BC%E3%83%88%E3%83%AD%E3%83%BC%E3%83%80%E3%83%BC%E3%81%AE%E3%83%93%E3%83%AB%E3%83%89
+
+export OS_DIR=/workspaces/mikanos
+
+cd /home/vscode/edk2
+
+# https://twitter.com/ww40336161/status/1380312835968835584
+git checkout edk2-stable202102
+
+if [ ! -d MikanLoaderPkg ]; then
+    ln -s $OS_DIR/MikanLoaderPkg ./
+fi
+
+source ./edksetup.sh
+
+sed -i '/ACTIVE_PLATFORM/ s:= .*$:= MikanLoaderPkg/MikanLoaderPkg.dsc:' Conf/target.txt
+sed -i '/TARGET_ARCH/ s:= .*$:= X64:' Conf/target.txt
+sed -i '/TOOL_CHAIN_TAG/ s:= .*$:= CLANG38:' Conf/target.txt
+
+sed -i '/CLANG38/ s/-flto//' Conf/tools_def.txt
+
+build
+
+export DISPLAY=host.docker.internal:0
+
+bash $OS_DIR/workspaces/mikanos/scripts/build_kernel.sh
+
+$HOME/osbook/devenv/run_qemu.sh Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf
