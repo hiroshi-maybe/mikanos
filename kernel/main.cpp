4853180db694488336f0ecedabde0499675b6f33
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-05-13 22:56:57 -0700

    Implement string writer

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -39,5 +39,7 @@ extern "C" void KernelMain(FrameBufferConfig& frame_buffer_config) {
         WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
     }
 
+    WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
+
     while(1) __asm__("hlt");
 }
