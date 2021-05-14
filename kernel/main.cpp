e7da922ea2973561a23a80d662472e455fd1f3c4
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-05-13 23:57:00 -0700

    Build to use sprintf with newlib support

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <cstddef>
+#include <cstdio>
 #include "frame_buffer_config.hpp"
 #include "font.hpp"
 #include "graphics.hpp"
@@ -40,6 +41,10 @@ extern "C" void KernelMain(FrameBufferConfig& frame_buffer_config) {
     }
 
     WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
+
+    char buf[128];
+    sprintf(buf, "1 + 2 = %d", 1 + 2);
+    WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
 
     while(1) __asm__("hlt");
 }
