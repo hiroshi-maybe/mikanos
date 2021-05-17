4aa66aeb2de6fead40962e6202a67f1193c9a05f
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-05-17 10:03:44 -0700

    Draw mouse cursor

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -6,6 +6,35 @@
 #include "font.hpp"
 #include "graphics.hpp"
 
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+  "@              ",
+  "@@             ",
+  "@.@            ",
+  "@..@           ",
+  "@...@          ",
+  "@....@         ",
+  "@.....@        ",
+  "@......@       ",
+  "@.......@      ",
+  "@........@     ",
+  "@.........@    ",
+  "@..........@   ",
+  "@...........@  ",
+  "@............@ ",
+  "@......@@@@@@@@",
+  "@......@       ",
+  "@....@@.@      ",
+  "@...@ @.@      ",
+  "@..@   @.@     ",
+  "@.@    @.@     ",
+  "@@      @.@    ",
+  "@       @.@    ",
+  "         @.@   ",
+  "         @@@   ",
+};
+
 void* operator new(size_t size, void* buf) {
     return buf;
 }
@@ -45,5 +74,16 @@ extern "C" void KernelMain(FrameBufferConfig& frame_buffer_config) {
     for (int i = 0; i < 27; ++i) {
         printk("printk: %d\n", i);
     }
+
+    for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
+        for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
+            if (mouse_cursor_shape[dy][dx] == '@') {
+                pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+            } else if (mouse_cursor_shape[dy][dx] == '.') {
+                pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+            }
+        }
+    }
+
     while(1) __asm__("hlt");
 }
